325ba5ca66081f0daa41fc8c26a82d11f228f646
yjzhang3 <yjzhang3@bu.edu>
2022-03-11 01:33:25 -0600

    workspace

--- fit_data.m
+++ fit_data.m
@@ -4,14 +4,14 @@ function x = fit_data(real_data,TF_conc_t,RNAp_conc,nbd)
 
 %% parameters
 rng default
-lb = zeros(11,1)-1E2; 
+lb = zeros(10,1)-1E2; 
 lb(1) = 1; % lower bound for RNAp concentration is nonzero
 
-ub = zeros(11,1)+1E+2; 
+ub = zeros(10,1)+1E+2; 
 
 fun = @(p) objective_function(real_data,TF_conc_t,RNAp_conc,nbd,p);
 
-nvars = 11; 
+nvars = 10; 
 
 %% particle swarm
 x = particleswarm(fun,nvars,lb,ub);
