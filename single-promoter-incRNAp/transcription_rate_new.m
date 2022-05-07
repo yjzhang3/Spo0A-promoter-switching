8f5f12f966196258ac07a1f9e391ba75f7683fb8
yjzhang3 <yjzhang3@bu.edu>
2022-05-06 20:16:25 -0500

    vmax as variable for each strain better version

--- single-promoter-incRNAp/transcription_rate_new.m
+++ single-promoter-incRNAp/transcription_rate_new.m
@@ -1,13 +1,17 @@
 function TR = transcription_rate_new(nbd,energyi,mut,TF_conc,RNAp_conc,vmax)
 
 % vmax is now an array
-
+% create a map that maps a unique vmax to each configuration
 bins = dec2bin(0:(2^nbd-1), nbd) - '0';
 on_config = bins(bins(:,end)==1,:);
 
-% keyset = {[0,0,0,1] [0,0,1,1] [0,1,0,1] [0,1,1,1] [1,0,0,1] [1,0,1,1] ...
-%     [1,1,0,1],[1,1,1,1]};
+for gg = 1:length(on_config)
+    str = string(on_config(gg,:));
+    curr_config = append(str(1),str(2),str(3));
+    keyset{gg} = convertStringsToChars(curr_config);
+end
 
+M = containers.Map(keyset,vmax);
 
 % consider mutated sites
 for ss = 1:length(mut)
@@ -18,10 +22,11 @@ end
 
 TR = 0;
 for oo = 1:length(on_config)
-    config_curr = on_config(oo,:);
-    config_type = bi2de(config_curr(1:3)); % the decimal representation of the configuration
-    curr = vmax(config_type+1)*prob_per_config_new(nbd,on_config(oo,:),energyi,mut,TF_conc,RNAp_conc);
-    TR = TR + curr;
+    str = string(on_config(oo,:));
+    curr_config = convertStringsToChars(append(str(1),str(2),str(3)));
+    vmax = M(curr_config);
+    curr_tr = vmax*prob_per_config_new(nbd,on_config(oo,:),energyi,mut,TF_conc,RNAp_conc);
+    TR = TR + curr_tr;
 end
 
 TR = TR/(2^(nbd-1-sum(mut)));
